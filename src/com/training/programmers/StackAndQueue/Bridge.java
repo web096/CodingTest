4b93bbf49e9cad458114d092932da27da7023488
web096 <web096@gmail.com>
2021-05-02 21:04:05 +0900

    풀이중

--- src/com/training/programmers/StackAndQueue/Bridge.java
+++ src/com/training/programmers/StackAndQueue/Bridge.java
@@ -1,5 +1,8 @@
 package com.training.programmers.StackAndQueue;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Bridge {
 
     public static void main(String[] args) {
@@ -14,6 +17,26 @@ public class Bridge {
 
     public static int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
+
+        Queue<Integer> queue = new LinkedList();
+
+        for(int i = 0; i < bridge_length;i++){
+            queue.add(0);
+        }
+
+        int currentWeight = truck_weights[0];
+        queue.add(currentWeight);
+        int currentSecend = 1; //다리위에 첫번째 트럭이 올라가 있기 때문에 1초가 사용됐다.
+        int nextIndex = 1; //다리위에 첫번째 트럭이 올라가 있기 때문에 nextIndex는 다음 index를 표시해준다.
+System.out.println(queue.size());
+
+        while (!queue.isEmpty()){
+
+            int truck = queue.poll();
+            currentWeight -= truck;
+
+
+        }
 
         return answer;
     }
